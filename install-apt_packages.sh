1b94872fdf1b02f551f8e3295056a4221fac754d
Danny Park <dpark@broadinstitute.org>
2021-04-28 00:27:55 -0400

    tolerate weird exit codes from script

--- install-apt_packages.sh
+++ install-apt_packages.sh
@@ -5,13 +5,15 @@ set -e
 # Add some basics
 apt-get update
 apt-get install -y -qq --no-install-recommends \
-	ca-certificates locales dirmngr \
+	ca-certificates locales dirmngr apt-utils \
 	less nano vim git wget curl jq parallel \
 	pigz zstd \
 	python3 python3-pip python3-crcmod
 
 # add source for node.js 14
+set +e
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
+set -e
 apt-get install -y -qq --no-install-recommends \
 	nodejs npm
 
