e97daf03cabba5583fb3d853572d86b74ccce338
Danny Park <dpark@broadinstitute.org>
2021-04-28 00:19:51 -0400

    remove pipefail

--- install-apt_packages.sh
+++ install-apt_packages.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-set -e -o pipefail
-
-apt-get update
-apt-get install -y -qq --no-install-recommends curl
-
-# add source for node.js 14
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+set -e
 
 # Add some basics
 apt-get update
@@ -14,11 +8,15 @@ apt-get install -y -qq --no-install-recommends \
 	ca-certificates locales dirmngr \
 	less nano vim git wget curl jq parallel \
 	pigz zstd \
-	python3 python3-pip python3-crcmod \
+	python3 python3-pip python3-crcmod
+
+# add source for node.js 14
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y -qq --no-install-recommends \
 	nodejs npm
 
 # Upgrade and clean
-apt-get upgrade -y
+#apt-get upgrade -y
 apt-get clean
 
 locale-gen en_US.UTF-8
